21b8bd6d148eb07fb54fad300a70710f3818e07a
sunhwa <2017sunhwa@gmail.com>
2021-10-30 23:20:25 +0900

    🐛 ⚡️ [feat]top, footer, gnb 구성파일, swc 버그수정

---
+++ src/component/Gnb.js
@@ -0,0 +1,11 @@
+export default function Gnb() {
+  return (
+    <div>
+      <div className="ui inverted menu">
+        <a className="active item">Home</a>
+        <a className="item">Messages</a>
+        <a className="item">Friends</a>
+      </div>
+    </div>
+  );
+}
