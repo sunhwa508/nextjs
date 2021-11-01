b533239c70700848af8a81c503bdb89ab32048a4
sunhwa <2017sunhwa@gmail.com>
2021-11-01 23:11:23 +0900

    ✨ [feat]home 화면 로딩화면 추가

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,16 @@ import { Header, Divider } from "semantic-ui-react";
 
 export default function Home() {
   const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const API_URL =
     "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 
   function getDate() {
-    axios.get(API_URL).then((res) => setList(res.data));
+    axios.get(API_URL).then((res) => {
+      setList(res.data);
+      setIsLoading(false);
+    });
   }
 
   useEffect(() => {
@@ -22,17 +27,30 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>HOME | 프론트몽키</title>
+        <meta name="description" content="프론트몽키 홈 입니다."></meta>
       </Head>
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        베스트상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(0, 6)} />
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        신상품
-      </Header>
-      <Divider />
-      <ItemList list={list} />
+      {isLoading && (
+        <div style={{ padding: "300px 0" }} className="ui segment">
+          <div className="ui active inverted dimmer">
+            <div className="ui text loader">Loading</div>
+          </div>
+          <p></p>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            베스트상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(0, 6)} />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <Divider />
+          <ItemList list={list} />
+        </>
+      )}
     </div>
   );
 }
