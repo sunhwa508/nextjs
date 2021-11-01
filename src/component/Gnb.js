9fec8930bcaeef6974345d169d21db3f83e9c78a
sunhwa <2017sunhwa@gmail.com>
2021-11-01 23:12:13 +0900

    ✨ [feat]menu link , active 추가

--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -1,10 +1,26 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Gnb() {
+  const router = useRouter();
+
   return (
     <div>
       <div className="ui inverted menu">
-        <a className="active item">Home</a>
-        <a className="item">Messages</a>
-        <a className="item">Friends</a>
+        <Link href="/">
+          <a className={`${router.pathname === "/" ? "active item" : "item"}`}>
+            Home
+          </a>
+        </Link>
+        <Link href="/about">
+          <a
+            className={`${
+              router.pathname === "/about" ? "active item" : "item"
+            }`}
+          >
+            About
+          </a>
+        </Link>
       </div>
     </div>
   );
