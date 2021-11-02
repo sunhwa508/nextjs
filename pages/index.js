f4c0c8e6ddab963636f490c40398b874e0d1e557
sunhwa <2017sunhwa@gmail.com>
2021-11-02 22:02:09 +0900

    ♻️ [refactor]정적생성 코드 작성

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,43 @@
-import { useState, useEffect } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
 import ItemList from "../src/component/ItemList";
 import { Header, Divider } from "semantic-ui-react";
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  // 브라우저 환경이라 next_public_ 를 붙임
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  function getDate() {
-    axios.get(API_URL).then((res) => {
-      setList(res.data);
-      setIsLoading(false);
-    });
-  }
-
-  useEffect(() => {
-    getDate();
-  }, []);
-
+export default function Home({ list }) {
+  // 페이지가 바로 나타나기 때문에 loading 화면이 필요하지 않음
   return (
     <div className={styles.container}>
       <Head>
         <title>HOME | 프론트몽키</title>
         <meta name="description" content="프론트몽키 홈 입니다."></meta>
       </Head>
-      {isLoading && (
-        <div style={{ padding: "300px 0" }} className="ui segment">
-          <div className="ui active inverted dimmer">
-            <div className="ui text loader">Loading</div>
-          </div>
-          <p></p>
-        </div>
-      )}
-      {!isLoading && (
-        <>
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            베스트상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(0, 6)} />
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            신상품
-          </Header>
-          <Divider />
-          <ItemList list={list} />
-        </>
-      )}
+      <>
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          베스트상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(0, 6)} />
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          신상품
+        </Header>
+        <Divider />
+        <ItemList list={list} />
+      </>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  // 클라이언트 환경이 아니기 때문에 NEXT_PUBLIC 을 붙힐 필요 없음
+  const apiUrl = process.env.apiUrl;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
 }
