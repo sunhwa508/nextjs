a3c80c582bb18713fca516534894e59db45ec498
sunhwalee <61695175+sunhwa508@users.noreply.github.com>
2022-05-18 08:50:40 +0900

    Update [id].js
    설명 추가

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -15,6 +15,7 @@ export default function Post({ item, name }) {
   );
 }
 
+// getServerSideProps는 데이터 요청시 인출해야 페이지를 미리 렌더링해야하는 경우 사용한다. 
 export async function getServerSideProps(context) {
   const id = context.params.id;
   const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
